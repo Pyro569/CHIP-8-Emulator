074eae3c4fc5b99095cdca578b8da3f763d57c1b
Pyro569 <doggolover98@outlook.com>
2024-03-11 21:52:39 -0400

    Basic Screen System Implementation

--- Emulator.cs
+++ Emulator.cs
@@ -1,4 +1,6 @@
-﻿namespace Chip8
+﻿using System.Diagnostics;
+
+namespace Chip8
 {
     class Emulator
     {
@@ -7,9 +9,15 @@
         static string programContent = "";
         static string currentOpcode;
 
+        public static bool emulating;
+
+        public static List<List<int>> screen = new List<List<int>>();
+
         public static void Main(string[] args)
         {
             CPU cpu = new CPU();
+            emulating = true;
+
             //get the binary input as an argument and read the contents of the binary
             program = args[0];
             StreamReader sr = new StreamReader(program);
@@ -19,19 +27,39 @@
             byte[] bytes = System.Text.Encoding.UTF8.GetBytes(programContent);
             programContent = Convert.ToHexString(bytes);
 
-            for (int i = 0; i < programContent.Length; i++)
+            //create the screen list through some magic math
+            for (int i = 0; i < 32; i++)
             {
-                //for some reason if i do this in one move it all ends up as numbers and i dont understand why
-                currentOpcode = programContent[i] + "";
-                currentOpcode += programContent[i + 1];
-                //increment i to account for the extra pc counter & increment pc
-                i += 1;
-                pc += 2;
+                List<int> row = new List<int>();
+                for (int k = 0; k < 65; k++)
+                    row.Add(0);
+                screen.Add(row);
+            }
 
-                decodeOpCode(currentOpcode);
+            replacePixel(0, 64, 1);
+
+            while (emulating)
+            {
+                Console.Clear();
+                drawScreen();
+
+                for (int i = 0; i < programContent.Length; i++)
+                {
+                    currentOpcode = programContent[i] + "" + programContent[i + 1];
+                    //currentOpcode += programContent[i + 1];
+                    //increment i to account for the extra pc counter & increment pc
+                    i += 1;
+                    pc += 2;
+
+                    //decodeOpCode(currentOpcode);
+                }
+
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
+
+                while (timer.Elapsed <= TimeSpan.FromSeconds(1)) ;
             }
         }
-
 
         public static void decodeOpCode(string opcode)
         {
@@ -42,6 +70,24 @@
                     Console.WriteLine("Clearing screen...");
                     break;
             }
+        }
+
+        public static void drawScreen()
+        {
+            for (int i = 0; i < screen.Count; i++)
+            {
+                for (int k = 0; k < screen[i].Count; k++)
+                    if (screen[i][k] == 1)
+                        Console.Write("■");
+                    else
+                        Console.Write(" ");
+                Console.Write("\n");
+            }
+        }
+
+        public static void replacePixel(int px, int py, int status)
+        {
+            screen[px][py] = status;
         }
     }
 }
