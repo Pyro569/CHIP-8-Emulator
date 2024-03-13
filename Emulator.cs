b99a326087aad6b10328c89da0994327d766c771
Pyro569 <doggolover98@outlook.com>
2024-03-12 20:38:10 -0400

    1NNN Opcode

--- Emulator.cs
+++ Emulator.cs
@@ -14,6 +14,8 @@ namespace Chip8
         public static bool emulating;
 
         public static List<List<int>> screen = new List<List<int>>();
+        public static List<string> OpCodes = new List<string>();
+        static List<string> devOutputs = new List<string>();
 
         public static void Main(string[] args)
         {
@@ -30,10 +32,10 @@ namespace Chip8
             programContent = Convert.ToHexString(bytes);
 
             //create the screen list through some magic math
-            for (int i = 0; i < 32; i++)
+            for (int i = 0; i < 33; i++)
             {
                 List<int> row = new List<int>();
-                for (int k = 0; k < 64; k++)
+                for (int k = 0; k < 65; k++)
                     row.Add(0);
                 screen.Add(row);
             }
@@ -50,16 +52,20 @@ namespace Chip8
             for (int i = 0; i < programContent.Length; i++)
             {
                 currentOpcode = programContent[i] + "" + programContent[i + 1];
-                //currentOpcode += programContent[i + 1];
-                //increment i to account for the extra pc counter & increment pc
+                Console.WriteLine("Current opcode: " + currentOpcode);
+                OpCodes.Add(currentOpcode);
+                //increment i to account for the extra byte
                 i += 1;
-                pc += 2;
-
-                decodeOpCode(currentOpcode);
             }
+
+            for (int i = 0; i < OpCodes.Count; i++)
+                decodeOpCode(OpCodes[i], i);
+
+            for (int i = 0; i < devOutputs.Count; i++)
+                Console.WriteLine(devOutputs[i]);
         }
 
-        public static void decodeOpCode(string opcode)
+        public static void decodeOpCode(string opcode, int opcodeIndex)
         {
             switch (opcode[0])
             {
@@ -67,10 +73,27 @@ namespace Chip8
                     if (opcode[1] == '0')
                     {
                         Console.Clear();
-                        Console.WriteLine("Clearing screen...");
+                        devOutputs.Add("Clearing screen...");
+                        pc += 2;
                     }
                     break;
-                case '0':
+                case '1':
+                    int[] hexDigits = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+                    int hexDigit = Convert.ToInt32(opcode[1].ToString(), 16);
+
+                    if (hexDigits.Contains(hexDigit))
+                    {
+                        pc += 2;
+                        int hexDigit1 = Convert.ToInt32(OpCodes[opcodeIndex + 1][0].ToString(), 16);
+                        int hexDigit2 = Convert.ToInt32(OpCodes[opcodeIndex + 1][1].ToString(), 16);
+
+                        if (hexDigits.Contains(hexDigit1) && hexDigits.Contains(hexDigit2))
+                        {
+                            devOutputs.Add("Setting PC to " + opcode[1] + OpCodes[opcodeIndex + 1]);
+                            pc = Convert.ToInt32(opcode[1] + OpCodes[opcodeIndex + 1], 16);
+                            pc += 2;
+                        }
+                    }
                     break;
             }
         }
